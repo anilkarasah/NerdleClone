b86b015c5e33f14aeaac9a0d1ad1a23b22f35d50
Anıl Karaşah <anilkarasah@hotmail.com>
2022-04-09 21:03:28 +0300

    Trying out Intellij Idea's commit/push/pull features

--- src/Nerdle/Nerdle.java
+++ src/Nerdle/Nerdle.java
@@ -2,8 +2,8 @@ package Nerdle;
 
 public class Nerdle {
     public static void main(String[] args) {
-        Equation eq = new Equation();
-        
+//        Equation eq = new Equation();
+
         System.out.println("Hello, world!");
     }
 }
